d9ea45562a69b219c7f0d2b28a79bbd700e70353
Hongyu Zhang <hongyu.zhang@intel.com>
2016-05-12 16:08:27 +0800

    Disable auto launch.

--- java/com/example/hongyu/jiangwen/BootBroadCastReceiver.java
+++ java/com/example/hongyu/jiangwen/BootBroadCastReceiver.java
@@ -3,6 +3,7 @@ package com.example.hongyu.jiangwen;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 /**
  * Created by hzhan72 on 2016/4/28.
@@ -14,7 +15,9 @@ public class BootBroadCastReceiver extends BroadcastReceiver {
         if (intent.getAction().equals(ACTION)) {
             Intent intent2 = new Intent(context, MainActivity.class);
             intent2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(intent2);
+            // start new activity
+            // context.startActivity(intent2);
+            Log.d("JiangWen", "BOOT_COMPLETE message received");
         }
     }
 }
