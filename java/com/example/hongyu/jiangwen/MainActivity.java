042830cb453bf52aaded6c311a9020833c1f841a
Hongyu Zhang <hongyu.zhang@intel.com>
2016-05-13 12:02:35 +0800

    update background

--- java/com/example/hongyu/jiangwen/MainActivity.java
+++ java/com/example/hongyu/jiangwen/MainActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
